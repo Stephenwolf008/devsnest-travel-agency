5a7182cb703840365329853d706ebc2c7f8f1d1f
Gaganmeet Bahri <gaganmeet.bahri96@gmail.com>
2021-07-18 17:37:19 +0530

    card and modal added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Card from "./Card";
 
 function App() {
+  // fetch data from https:localhost:5000/data using useEffect hook
+  useEffect(() => {
+    fetch("http://localhost:5000/data")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+  const [data, setData] = useState([]);
+  console.log(data);
   return (
     <div className="App">
-      <Card />
+      <section className="cards">
+        {data.map((card) => (
+          <Card
+            key={card.name}
+            name={card.place_name}
+            attractions={card.attractions.length}
+            description={card.description}
+            img={card.attractions[0].url}
+          />
+        ))}
+      </section>
     </div>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -1,31 +1,72 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
 import "./Card.css";
-const Card = () => {
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+const Card = ({ name, attractions, description, img }) => {
+  const style = {
+    backgroundImage: `url("${img}")`,
+  };
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
-    <div className="card">
-      <div className="card-image"> </div>
-      <div className="card-text">
-        <span class="date">4 days ago</span>
-        <h2>Post One</h2>
-        <p> This is a card div for state of haryana </p>
-      </div>
-      <div className="card-stats">
-        <div className="stat">
-          <div className="value">
-            4<sup>m</sup>
+    <>
+      <div className="card" onClick={handleOpen}>
+        <div className="card-image" style={style}></div>
+        <div className="card-text">
+          <span className="date">4 days ago</span>
+          <h2>{name}</h2>
+          <p> {description}</p>
+        </div>
+        <div className="card-stats">
+          <div className="stat">
+            <div className="value">{attractions}</div>
+            <div className="type">Attractions</div>
           </div>
-          <div className="type">read</div>
-        </div>
-        <div className="stat border">
-          <div className="value">5123</div>
-          <div className="type">views</div>
-        </div>
-        <div className="stat">
-          <div className="value">32</div>
-          <div className="type">comments</div>
         </div>
       </div>
-    </div>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <h2 id="transition-modal-title">{name}</h2>
+            <p id="transition-modal-description">{description}</p>
+          </div>
+        </Fade>
+      </Modal>
+    </>
   );
 };
 
